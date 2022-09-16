a13c0e131028eba90fed4379c6939d7b7828111d
Wei Wang <weiwangmeta@fb.com>
2022-09-16 16:24:20 +0000

    test

--- tools/scripts/generate_binary_build_matrix.py
+++ tools/scripts/generate_binary_build_matrix.py
@@ -22,7 +22,7 @@ FULL_PYTHON_VERSIONS = ["3.9"]
 ROCM_ARCHES = ["5.1.1", "5.2"]
 CUDA_ACRHES_DICT = {
 #    "nightly": ["10.2", "11.3", "11.6", "11.7"],
-    "nightly": ["11.6"],
+    "nightly": [],
     "test": ["10.2", "11.3", "11.6"],
     "release": ["10.2", "11.3", "11.6"]
 }
